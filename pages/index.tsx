5788d37187815c0c9c033b6cae9ee12cf63de1e1
Rasmez <ricardo.cdl.vr@gmail.com>
2021-10-16 15:24:03 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import * as React from 'react';
+
+const Home: NextPage = () => {
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  )
+}
+
+export default Home
