cc22d7e6980647e9492790489590a151c687f204
Rasmez <ricardo.cdl.vr@gmail.com>
2021-11-06 05:45:04 -0400

    Initial Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
 import * as React from 'react';
+import Notes from '../components/Notes';
+import Create from '../components/Create';
 
 const Home: NextPage = () => {
   return (
     <React.Fragment>
+      <Notes />
+      <Create />
     </React.Fragment>
   )
 }
